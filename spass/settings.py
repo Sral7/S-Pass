1e2a65ea9ddc5f475d49d07fdde4b46bfa95ed61
Sral7 <143665527+Sral7@users.noreply.github.com>
2023-09-24 02:28:41 +0200

    started adding css elements

--- spass/settings.py
+++ spass/settings.py
@@ -143,8 +143,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
