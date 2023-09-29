e3d2f8d0608d8c0229471b1b5a44a1ccbea85c91
Sral7 <143665527+Sral7@users.noreply.github.com>
2023-09-29 10:32:38 +0200

    debugging

--- spass/settings.py
+++ spass/settings.py
@@ -124,7 +124,6 @@ DATABASES = {
 }
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
