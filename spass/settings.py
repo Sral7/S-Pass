1d69be67343341e6023fad8449b6b6afafc190d3
Sral7 <143665527+Sral7@users.noreply.github.com>
2023-09-29 08:42:09 +0200

    settings change for heroku deploy

--- spass/settings.py
+++ spass/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ['*']
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'login',
     'manager',
     'cryptography',
+    'django_heroku',
 ]
 
 # settings.py
@@ -137,8 +138,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
