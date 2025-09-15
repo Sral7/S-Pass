61a2781245de11a3324ed1dc0c1c60294c15dbc9
Sral7 <143665527+Sral7@users.noreply.github.com>
2025-09-15 13:47:39 +0200

    Update settings.py

--- spass/settings.py
+++ spass/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://s-pass-1.onrender.com/']
 
 
 # Application definition
